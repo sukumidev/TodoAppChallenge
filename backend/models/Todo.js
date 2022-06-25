1a391042a803f4de209c2664785d8b1129c3478c
sukumidev <105608015+sukumidev@users.noreply.github.com>
2022-06-24 23:06:02 -0500

    Create Todo.js

---
+++ backend/models/Todo.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const TodoSchema = new mongoose.Schema({
+  text:{type:String,required:true},
+  done:{type:mongoose.SchemaTypes.Boolean,required:true},
+  user:{type:mongoose.SchemaTypes.ObjectId},
+  datedue:{type:String}
+});
+
+const Todo = mongoose.model('Todo', TodoSchema);
+
+export default Todo;
