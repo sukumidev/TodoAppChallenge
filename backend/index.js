d33446ba8bfa785c2b1d7582bbf2d1c81fc6b401
sukumidev <105608015+sukumidev@users.noreply.github.com>
2022-06-24 23:07:11 -0500

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,135 @@
+import express from 'express';
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import User from "./models/User.js";
+import Todo from "./models/Todo.js";
+import bcrypt from 'bcrypt';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+
+const secret = 'secret123';
+
+await mongoose.connect('mongodb://localhost:27017/auth', {useNewUrlParser:true, useUnifiedTopology:true});
+const db = mongoose.connection;
+db.on('error', console.log);
+
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json({extended:true}));
+app.use(cors({
+  credentials:true,
+  origin: 'http://localhost:3000',
+}));
+
+app.get('/', (req, res) => {
+  res.send('ok');
+});
+
+app.get('/user', (req, res) => {
+  if (!req.cookies.token) {
+    return res.json({});
+  }
+  const payload = jwt.verify(req.cookies.token, secret);
+  User.findById(payload.id)
+    .then(userInfo => {
+      if (!userInfo) {
+        return res.json({});
+      }
+      res.json({id:userInfo._id,email:userInfo.email});
+    });
+
+});
+
+app.post('/signup', (req, res) => {
+    const {username,email,password} = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    User.findOne({email})
+    .then(userInfo => {
+        if(userInfo){
+            console.log("Ya se registro este correo")
+            return res.sendStatus(500);
+        }
+        const user = new User({password:hashedPassword,email, username});
+        console.log("Si entra");
+        console.log(user);
+        user.save().then(userInfo => {
+        jwt.sign({id:userInfo._id,email:userInfo.email}, secret, (err,token) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+        } else {
+          res.cookie('token', token).json({id:userInfo._id,email:userInfo.email, username:userInfo.username});
+          return res.sendStatus(200);
+        }
+      });
+    });
+    });
+  });
+
+app.post('/login', (req, res) => {
+  const {email,password} = req.body;
+  User.findOne({email})
+    .then(userInfo => {
+      if (!userInfo) {
+        return res.json({});
+      }
+      const passOk = bcrypt.compareSync(password, userInfo.password);
+      if (passOk) {
+        console.log("Si se loguea");
+        jwt.sign({id:userInfo._id,email},secret, (err,token) => {
+          if (err) {
+            console.log(err);
+            res.sendStatus(500);
+          } else {
+            res.cookie('token', token).json({id:userInfo._id,email:userInfo.email});
+          }
+        });
+      } else {
+        console.log("Credenciales incorrectas");
+        res.sendStatus(401);
+      }
+    })
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').send();
+});
+
+app.get('/todos', (req,res) => {
+    const payload = jwt.verify(req.cookies.token, secret);
+    Todo.where({user:new mongoose.Types.ObjectId(payload.id)})
+      .find((err,todos) => {
+        res.json(todos);
+      })
+  });
+
+  app.put('/todos', (req, res) => {
+    const payload = jwt.verify(req.cookies.token, secret);
+    const todo = new Todo({
+      text:req.body.text,
+      done:false,
+      user:new mongoose.Types.ObjectId(payload.id),
+      datedue:req.body.date
+    });
+    todo.save().then(todo => {
+      res.json(todo);
+    })
+  });
+  
+  app.post('/todos', (req,res) => {
+    const payload = jwt.verify(req.cookies.token, secret);
+    Todo.updateOne({
+      _id:new mongoose.Types.ObjectId(req.body.id),
+      user:new mongoose.Types.ObjectId(payload.id)
+    }, {
+      done:req.body.done,
+    }).then(() => {
+      res.sendStatus(200);
+    });
+  });
+  
+
+app.listen(4000, function(){
+    console.log("Express started in port 4000");
+});
